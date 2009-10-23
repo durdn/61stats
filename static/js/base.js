9adfa8459f1caea4e35dca927d99c3b31128f117
Nicola Paolucci <durden@gmail.com>
2009-10-24 00:46:29 +0200

    using new js lib for dynamic table

--- static/js/base.js
+++ static/js/base.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   spinning_gif = '<img src="/static/img/89.gif" />';
   if (!window.console) window.console = {};
   if (!window.console.log) window.console.log = function() {};
+  //data table connect
+  $('#bumps-table').dataTable();
+
+  //sammy routes definitions
   app = $.sammy(function() { with(this) {
     get('#/user/:username/:reload', function() { with(this) {
       //$('#reputation-bumps').html(spinning_gif);
@@ -13,6 +17,12 @@ $(document).ready(function() {
       //$('#reputation-bumps').html(spinning_gif);
       $.get('/user/' + params['username'], function (data) {
         $('#reputation-bumps').html(data).fadeIn();
+        $('#bumps-table').dataTable({
+          "bSort": true,
+          "bStateSave": true,
+          "sPaginationType": "full_numbers",
+          "aaSorting": [[0,'desc'], [1,'asc']]
+        });
       });
     }});
   }});
